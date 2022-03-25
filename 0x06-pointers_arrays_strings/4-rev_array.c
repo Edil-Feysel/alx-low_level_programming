7e57b36afe32a0b99bd05f5139a7597ad35255db
Edil-Feysel <enebgirl18@gmail.com>
2022-03-25 12:02:16 -0700

    reverse

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -3,6 +3,7 @@
  * reverse_array - reverses the content of an array of integers
  * @a: an array of integers
  * @n: number of elements of the array
+ * Return: returns void
  */
 
 void reverse-array(int *a, int n)
@@ -26,6 +27,7 @@ void reverse-array(int *a, int n)
 
 	for (i = 0; i < n / 2; i++)
 	{
+
 
 		c = *end_c;
 		*end_c = *start_c;
